e38eef34d95546ba100acd94266ef1c674c56c4e
김시우 <tldn12gh@gmail.com>
2022-08-02 02:01:52 +0900

    RESTful API 기초 프로젝트

---
+++ src/main/java/com/example/restfulwebservice/HelloWorldController.java
@@ -0,0 +1,9 @@
+package com.example.restfulwebservice;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    // GET
+    // /hello-wro
+}
