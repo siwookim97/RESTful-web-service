db54e694fb70855c01944df6ed9dbd9590aa82bf
김시우 <tldn12gh@gmail.com>
2022-08-05 03:46:54 +0900

    JPA의 사용을 위해 JpaRepository를 상속받을 Repository 인터페이스를 생성하고 이 인터페이스의 기능을 구현할 JPA Controller를 구현

---
+++ src/main/java/com/example/restfulwebservice/user/UserJpaController.java
@@ -0,0 +1,44 @@
+package com.example.restfulwebservice.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
+@RestController
+@RequestMapping("jpa/")
+public class UserJpaController {
+    @Autowired
+    private UserRepository userRepository;
+
+    // Get - jpa/users
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public Resource<User> retrieveUser(@PathVariable int id) {
+        Optional<User> user =  userRepository.findById(id);
+
+        if (!user.isPresent()) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+        // HATEOAS로도 사용이 가능하다
+        Resource<User> resource = new Resource<>(user.get());
+        ControllerLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        resource.add(linkTo.withRel("all-users"));
+
+        return resource;
+    }
+
+}
