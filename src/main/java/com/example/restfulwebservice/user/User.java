45295a481839bde173ed069fa006351fe4e6dd87
김시우 <tldn12gh@gmail.com>
2022-08-05 00:10:46 +0900

    JPA의 초기 설정값을 지정해주기 위한 *.sql파일

--- src/main/java/com/example/restfulwebservice/user/User.java
+++ src/main/java/com/example/restfulwebservice/user/User.java
@@ -7,6 +7,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
@@ -16,7 +19,11 @@ import java.util.Date;
 // @JsonIgnoreProperties(value={"password"})
 @NoArgsConstructor // 상속을 하기 위해 default 생성자 추가
 //@JsonFilter("UserInfo") // filter를 통해서
+//@ApiModel
+@Entity // JPA
 public class User {
+    @Id
+    @GeneratedValue
     private Integer id;
 
     @Size(min=2, message = "Name은 2글자 이상 입력해주세요")
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user values(1, sysdate(), 'name1', 'pass1', '111111-1111111');
+insert into user values(2, sysdate(), 'name2', 'pass2', '222222-2222222');
+insert into user values(3, sysdate(), 'name3', 'pass3', '333333-3333333');
