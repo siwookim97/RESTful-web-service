1a356e1f2ea264ee536401ba2f56b95d5918c95d
김시우 <tldn12gh@gmail.com>
2022-08-05 22:34:18 +0900

    Post Entity의 초기값 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,6 @@
-insert into user values(1, sysdate(), 'name1', 'pass1', '111111-1111111');
-insert into user values(2, sysdate(), 'name2', 'pass2', '222222-2222222');
-insert into user values(3, sysdate(), 'name3', 'pass3', '333333-3333333');
+insert into user values(90001, sysdate(), 'name1', 'pass1', '111111-1111111');
+insert into user values(90002, sysdate(), 'name2', 'pass2', '222222-2222222');
+insert into user values(90003, sysdate(), 'name3', 'pass3', '333333-3333333');
+
+insert into post values(10001, 'My first post', 90001);
+insert into post values(10002, 'My second post', 90001);
